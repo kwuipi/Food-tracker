897107bd78d14656547d9faf901518f664d32eb2
kwuipi <drakzon1@gmail.com>
2018-01-03 11:44:52 +0300

    added function "selectImageFromPhotoLibrary" to ViewController.swift

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate {
     
     //MARK: Properties
 
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        nameTextField.delegate = self as! UITextFieldDelegate
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
