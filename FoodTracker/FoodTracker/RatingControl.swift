04dce051fb484a9cfc139f98e3d7f5ca0cb0b1d9
kwuipi <drakzon1@gmail.com>
2018-01-03 15:31:23 +0300

    Added subclass "RatingControl"

---
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Евгений Таран on 1/3/18.
+//  Copyright © 2018 Zhenya Taran. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
